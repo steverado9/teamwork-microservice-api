bcefc9f3ac106d5289c350210003c79a8998f4e7
stephen <isaacsteverado9@gmail.com>
2026-08-02 01:57:11 +0100

    connected the user service to the eureka server

---
+++ teamwork-service-discovery/src/test/java/com/steverado/teamwork_service_discovery/TeamworkServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steverado.teamwork_service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamworkServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
